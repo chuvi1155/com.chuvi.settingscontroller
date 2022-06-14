ea85a9aecd93ee667a2e4568cc00b78a4189db8e
Stanislav Blinov <chuvi1155@users.noreply.github.com>
2022-06-14 21:15:54 +0300

    Update SettingsController.cs

--- Runtime/SettingsController.cs
+++ Runtime/SettingsController.cs
@@ -115,7 +115,7 @@ public class SettingsController : MonoBehaviour
     }
 
     /// <summary>
-    /// ≈ÒÎË ÔÓÎÂ settings ‡‚ÂÌ null
+    /// √Ö√±√´√® √Ø√Æ√´√• settings √∞√†√¢√•√≠ null
     /// </summary>
     public void Clear()
     {
@@ -141,9 +141,10 @@ public class SettingsController : MonoBehaviour
             if (settings is UserINISetting ini)
             {
                 var title = Background.Find<TMPro.TextMeshProUGUI>("MainTitle");
-                title.text = $"Õ¿—“–Œ… » ({ini.FileName})";
+                title.text = $"√ç√Ä√ë√í√ê√é√â√ä√à ({ini.FileName})";
                 title.rectTransform.sizeDelta = new Vector2(460, 50);
                 title.rectTransform.anchorMin = title.rectTransform.anchorMax = new Vector2(0.5f, 1f);
+                title.rectTransform.anchoredPosition = new Vector2(0f, -40f); 
                 Init(settings);
                 Background.SetActive(true);
             }
@@ -154,9 +155,10 @@ public class SettingsController : MonoBehaviour
             if (settings is UserXMLSettings xml)
             {
                 var title = Background.Find<TMPro.TextMeshProUGUI>("MainTitle");
-                title.text = $"Õ¿—“–Œ… » ({xml.FileName})";
+                title.text = $"√ç√Ä√ë√í√ê√é√â√ä√à ({xml.FileName})";
                 title.rectTransform.sizeDelta = new Vector2(460, 50);
                 title.rectTransform.anchorMin = title.rectTransform.anchorMax = new Vector2(0.5f, 1f);
+                title.rectTransform.anchoredPosition = new Vector2(0f, -40f); 
                 Init(settings);
                 Background.SetActive(true);
             }
